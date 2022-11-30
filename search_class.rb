a5ad1e5d4fa97b1d0817e23e8501675a712f2ac1
AntonTmk <35644424+AntonTmk@users.noreply.github.com>
2022-11-30 20:40:36 +0200

    Fix syntax Fix moved file operations to separate class

--- search_class.rb
+++ search_class.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'car_request'
+require_relative 'file_process'
 
 # class to get the result of a search request
 class SearchClass
@@ -9,10 +10,10 @@ class SearchClass
   def initialize(req)
     @request = req
     require 'yaml'
-    car_array = YAML.safe_load(File.read('cars.yml'))
-    @result = car_array.find_all { |n| @request.car_eql?(n) }
+    car_list = FileProcess.new('cars.yml')
+    @result = car_list.file_content.find_all { |n| @request.car_eql?(n) }
     sort_result
-    result_quantity if @result.length > 0
+    result_quantity if @result.length.positive?
   end
 
   def print_result
@@ -37,103 +38,47 @@ class SearchClass
     @result = @result.reverse if @request.sort_direction.casecmp('asc') != 0
   end
 
+  DEFAULT_REQUESTS_QUANTITY = 1
   def result_quantity
     @request.total_quantity = @result.length
-    require 'yaml'
-    searches = YAML.load(File.read('searches.yml'))
-    if searches == false
-      searches = [@request.car_hash(1, @result.length)]
+    searches = FileProcess.new('searches.yml')
+    unless searches.file_content
+      @request.requests_quantity = DEFAULT_REQUESTS_QUANTITY
+      searches.file_content = [@request.car_hash(DEFAULT_REQUESTS_QUANTITY, @result.length)]
     else
-      edit_history(searches)
+      edit_history(searches.file_content)
     end
-    File.write('searches.yml', searches.to_yaml)
+    searches.update_content
   end
 
   def edit_history(searches)
-    if !searches.find { |v| v.to_s.casecmp(@request.car_hash(v[:requests_quantity], v[:total_quantity]).to_s) }.nil?
-      (0..searches.length - 1).each do |v|
-        next unless searches[v].to_s.casecmp(@request.car_hash(searches[v][:requests_quantity], searches[v][:total_quantity]).to_s).zero?
-
-        searches[v][:requests_quantity] += 1
-        searches[v][:total_quantity] = @request.total_quantity
-        @request.requests_quantity = searches[v][:requests_quantity]
-      end
+    if include_req?(searches, @request)
+      update_request(searches, @request)
     else
-      @request.requests_quantity = 1
-      searches << @request.car_hash(1, @result.length)
-    end
-  end
-end
-# frozen_string_literal: true
-
-require_relative 'car_request'
-
-# class to get the result of a search request
-class SearchClass
-  attr_accessor :request, :result
-
-  def initialize(req)
-    @request = req
-    require 'yaml'
-    car_array = YAML.safe_load(File.read('cars.yml'))
-    @result = car_array.find_all { |n| @request.car_eql?(n) }
-    sort_result
-    result_quantity if @result.length > 0
-  end
-
-  def print_result
-    puts "-------------------------------------------- \n Statistic: \n \n"
-    puts "Total Quanity: #{@request.total_quantity} \n \nRequests quantity:  #{@request.requests_quantity} "
-    print = @result.to_s.gsub(/["}\[]/, '').gsub(/[{\]]/, "\n--------------------------------------------\n")
-    print = print.gsub(/,/, "\n").gsub(/=>/, ': ')
-    puts print
-  end
-
-  private
-
-  def sort_result
-    @result = if @request.sort_option.casecmp('price') != 0
-                @result.sort_by do |v|
-                  d, m, y = v['date_added'].split('/')
-                  [y.to_i, m.to_i, d.to_i]
-                end
-              else
-                @result.sort_by { |v| v['price'] }
-              end
-    @result = @result.reverse if @request.sort_direction.casecmp('asc') != 0
-  end
-
-  def result_quantity
-    file_exist('searches.yml')
-    @request.total_quantity = @result.length
-    require 'yaml'
-    searches = YAML.load(File.read('searches.yml'))
-    if searches == false
-      @request.requests_quantity=1
-      searches = [@request.car_hash(1, @result.length)]
-    else
-      edit_history(searches)
-    end
-    File.write('searches.yml', searches.to_yaml)
-  end
-
-  def edit_history(searches)
-    if searches.select { |v| v.to_s.casecmp(@request.car_hash(v[:requests_quantity], v[:total_quantity]).to_s).zero? }.length > 0
-      (0..searches.length - 1).each do |v|
-        unless !searches[v].to_s.casecmp(@request.car_hash(searches[v][:requests_quantity], searches[v][:total_quantity]).to_s).zero?
-
-        searches[v][:requests_quantity] += 1
-        searches[v][:total_quantity] = @request.total_quantity
-        @request.requests_quantity = searches[v][:requests_quantity]
-        end
-      end
-    else
-      @request.requests_quantity = 1
-      searches << @request.car_hash(1, @result.length)
+      add_request(searches, @request)
     end
   end
 
-  def file_exist(var)
-    File.new(var, 'w') unless File.exists?(var)
+  def add_request(list, req)
+    req.requests_quantity = DEFAULT_REQUESTS_QUANTITY
+    list << req.car_hash(DEFAULT_REQUESTS_QUANTITY, @result.length)
+  end
+
+  def update_request(list, req)
+    (0..list.length - 1).each do |v|
+      next if compare_req?(list[v], req)
+
+      list[v][:requests_quantity] += 1
+      list[v][:total_quantity] = req.total_quantity
+      req.requests_quantity = list[v][:requests_quantity]
+    end
+  end
+
+  def include_req?(searches, request)
+    searches.select { |req| compare_req?(req, request) }.length.positive?
+  end
+
+  def compare_req?(req1, req2)
+    req1.to_s.casecmp(req2.car_hash(req1[:requests_quantity], req1[:total_quantity]).to_s).zero?
   end
 end
