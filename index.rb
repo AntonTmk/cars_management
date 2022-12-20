e6a6ce0c66a1e131293ab73bda838e4f28ea5c65
AntonTmk <35644424+AntonTmk@users.noreply.github.com>
2022-12-20 20:35:01 +0200

    Add log in logic

--- index.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'config/requirements'
-include TextTranslation
-
-TextTranslation.available_languages
-puts I18n.t('Welcome').colorize(:black).on_blue
-startMenu = StartMenu.new
-startMenu.call
