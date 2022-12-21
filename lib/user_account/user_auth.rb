f63555bd9c054e7e8c3b2fa9130b82f215240332
AntonTmk <35644424+AntonTmk@users.noreply.github.com>
2022-12-21 19:51:39 +0200

    fix translation

--- lib/user_account/user_auth.rb
+++ lib/user_account/user_auth.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require_relative 'auth_modules/account_login'
 require_relative 'auth_modules/log_out'
 require_relative 'auth_modules/create_account'
 
+# module contains all authorization modules
 module UserAuth
   extend AccountLogin
   extend LogOut
