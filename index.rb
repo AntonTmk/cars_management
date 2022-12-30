8afa67d0c0f72f7c3c19881520fade65af69bbb6
AntonTmk <35644424+AntonTmk@users.noreply.github.com>
2022-12-30 19:47:25 +0200

    Fix edit some definitions name Add base_menu class Fix syntax

--- index.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'config/requirements'
-include TextTranslation
-
-TextTranslation.available_languages
-puts I18n.t('Welcome').colorize(:black).on_blue
-StartMenu.new.call
